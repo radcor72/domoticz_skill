ab01d0b4d7ec7cf1eecaa0f2cd8ea2691ea221bb
Cornel Radulescu <36704985+radcor72@users.noreply.github.com>
2019-01-21 11:52:23 +0200

    Update Domoticz.py

--- Domoticz.py
+++ Domoticz.py
@@ -2,6 +2,8 @@
 import urllib
 import urllib.request
 import json
+import os
+import configparser
 import re
 from mycroft.util.log import getLogger
 
@@ -12,33 +14,39 @@ class Domoticz:
     """Class for controlling Domoticz."""
     def __init__(self, host, port, protocol, authentication, login, password):
         """Recover settings for accessing to Domoticz instance."""
-        self.host = host
-        self.port = port
-        protocol = protocol
-        authentication = authentication
-        if protocol:
-            self.protocol = "https"
-        else:
-            self.protocol = "http"
+        config_name = "conf.cfg"
+        config_file = os.path.join(os.path.dirname(__file__), config_name)
+        self.config = configparser.ConfigParser(allow_no_value=True)
+        self.config.sections()
+        self.config.read(config_file)
+        self.host = self.config["domoticz"]["host"]
+        self.port = self.config["domoticz"]["port"]
+        self.protocol = self.config["domoticz"]["protocol"]
+        authentication = self.config["domoticz"].getboolean('authentication')
         if authentication:
-            self.login = login
-            self.password = password
+            self.login = self.config["domoticz"]["login"]
+            self.password = self.config["domoticz"]["password"]
             self.url = self.protocol + "://" + self.login + ":" + self.password + "@" + self.host + ":" + self.port
         else:
             self.url = self.protocol + "://" + self.host + ":" + self.port
 
-    def findid(self, what, where, state):
+    def findid(self, what, where, state = None):
         i = 0
+        """Convert the 'what' and the 'where', for recover the idx of the device in Domoticz."""
+        idx = 0
+        if where is None: return [0,None,False,0]
+        for (key, val) in self.config.items("devices"):
+            if what.lower().strip() + "-" + where.lower().strip() == key.lower().strip():
+                idx=val
         wht = re.compile(what, re.I)
         whr = re.compile(where, re.I)
         f = urllib.request.urlopen(self.url + "/json.htm?type=devices&filter=all&used=true")
         response = f.read()
         payload = json.loads(response.decode('utf-8'))
-        idx = False
         stype = False
         dlevel = False
         while i < len(payload['result']):
-            if whr.search(payload['result'][i]['Name']) and wht.search(payload['result'][i]['Name']):
+            if idx==payload['result'][i]['idx']:
                 stype = payload['result'][i]['Type']
                 typ = re.compile(stype, re.I)
                 dlevel = "100"
@@ -49,17 +57,17 @@ class Domoticz:
                     dlevel = payload['result'][i]['Level']
                 else:
                     stype = "light"
-                idx = payload['result'][i]['idx']
                 rslt = re.compile(" " + str(state).title(), re.I)
                 if rslt.search(" " + payload['result'][i]['Data']):
                     result = 0
                 else:
                     result = 1
                 break
-            elif i is len(payload['result']) - 1:
+            else:
                 result = None
                 break
             i += 1
+        LOGGER.debug('findid results are: idx=' + str(idx) + ', result=' + str(result) + ', stype=' + str(stype) + ', dlevel=$
         return [idx, result, stype, dlevel]
 
     def findcmd(self, state, action, dlevel):
@@ -88,7 +96,7 @@ class Domoticz:
                 stlvl = 100
             cmd = "Set%20Level&level=" + str(stlvl)
         elif rslt2.search('set'):
-            stlvl = int(cmd)
+            stlvl = int(dsrdst)
             if stlvl > 100:
                 stlvl = 100
             elif stlvl < 0:
@@ -99,6 +107,7 @@ class Domoticz:
                 cmd = "On"
             elif rslt.search('unlock') or rslt.search('close') or rslt.search('off'):
                 cmd = "Off"
+        LOGGER.debug('findcmd string is: ' + str(cmd))
         return cmd
 
     def switch(self, state, what, where, action):
@@ -115,16 +124,21 @@ class Domoticz:
                 try:
                     f = urllib.request.urlopen(self.url + "/json.htm?type=command&param=switch" + stype + "&idx=" + str(idx) + "&switchcmd=" + str(cmd))
                     response = f.read()
-                    LOGGER.debug(str(response))
+                    LOGGER.debug('url  to open is: ' + self.url + "/json.htm?type=command&param=switch" + stype + "&idx=" + str(idx) + "&switchcmd=" + str(cmd))
+                    LOGGER.debug('response is: ' + str(response))
                     return response
                 except IOError as e:
                     LOGGER.error(str(e) + ' : ' + str(e.read()))
-        else:
-            LOGGER.debug("no command found")
+        elif result is 0: LOGGER.debug("no change to the device")
+        else: LOGGER.debug("no command found")
         return result
 
     def get(self, what, where):
         """Get the device's data in Domoticz."""
+        data = []
+        data = self.findid(what, where)
+        idx = data[0]
+        result = data[1]
         try:
             f = urllib.request.urlopen(self.url + "/json.htm?type=devices&filter=all&used=true")
             response = f.read()
@@ -134,7 +148,7 @@ class Domoticz:
             if where is not None:
                 whr = re.compile(where, re.I)
                 while i < len(payload['result']):
-                    if whr.search(payload['result'][i]['Name']) and wht.search(payload['result'][i]['Name']):
+                    if idx==payload['result'][i]['idx']:
                         break
                     elif i is len(payload['result']) - 1:
                         payload['result'][i]['Data'] = None
@@ -142,12 +156,17 @@ class Domoticz:
                     i += 1
             elif where is None:
                 while i < len(payload['result']):
-                    if wht.search(payload['result'][i]['Name']):
+                    if wht.search(payload['result'][i]['idx']):
                         break
                     elif i is len(payload['result']) - 1:
                         payload['result'][i]['Data'] = None
                         break
                     i += 1
-            return payload['result'][i]
+            if result is 1:
+                value =  payload['result'][i]['Data'].split(",")[0]
+            else:
+                value = None
+            LOGGER.debug('get payload value is:' + (value or 'None'))
+            return value
         except IOError as e:
             LOGGER.error(str(e) + ' : ' + str(e.read()))
